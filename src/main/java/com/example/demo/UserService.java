32efe2a761959ce5dbe683ed8376d01565f9175d
forensikk <sddivanov@yandex.ru>
2024-05-13 15:32:31 +0300

    second part

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.User;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 @Service
--- src/main/java/com/example/demo/UserServiceImpl.java
+++ src/main/java/com/example/demo/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(int id) {
-        return userRepository.findById(id).orElse(null);
+        return (User) userRepository.findById(id).orElse(null);
     }
 }
